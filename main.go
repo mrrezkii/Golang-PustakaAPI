e90615f3969c996c865bb4b4f969ca2f98a43536
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-04-03 03:41:14 +0700

    Handler

--- main.go
+++ main.go
@@ -8,19 +8,23 @@ import (
 func main() {
 	router := gin.Default()
 
-	router.GET("/", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"name": "Muhamamd Rezki Ananda",
-			"bio":  "Professional Software Engineer",
-		})
-	})
+	router.GET("/", rootHandler)
 
-	router.GET("/hello", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"title":    "Helo World",
-			"subtitle": "Belajar Golang bareng Agung Setiawan",
-		})
-	})
+	router.GET("/hello", helloHandler)
 
 	router.Run(":8888")
+}
+
+func rootHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"name": "Muhamamd Rezki Ananda",
+		"bio":  "Professional Software Engineer",
+	})
+}
+
+func helloHandler(c *gin.Context) {
+	c.JSON(http.StatusOK, gin.H{
+		"title":    "Helo World",
+		"subtitle": "Belajar Golang bareng Agung Setiawan",
+	})
 }
