0c2a09234048eb16b6244960714173cfe0c528c8
Muhammad Rezki Ananda <muhammad.rezki.ananda@gmail.com>
2022-04-03 12:24:59 +0700

    Path variable and query string

--- main.go
+++ main.go
@@ -9,8 +9,9 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", rootHandler)
-
 	router.GET("/hello", helloHandler)
+	router.GET("/books/:id/:title", booksHandler)
+	router.GET("/query", queryHandler)
 
 	router.Run(":8888")
 }
@@ -26,5 +27,23 @@ func helloHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"title":    "Helo World",
 		"subtitle": "Belajar Golang bareng Agung Setiawan",
+	})
+}
+
+func booksHandler(c *gin.Context) {
+	id := c.Param("id")
+	title := c.Param("title")
+	c.JSON(http.StatusOK, gin.H{
+		"id":    id,
+		"title": title,
+	})
+}
+
+func queryHandler(c *gin.Context) {
+	title := c.Query("title")
+	price := c.Query("price")
+	c.JSON(http.StatusOK, gin.H{
+		"title": title,
+		"price": price,
 	})
 }
